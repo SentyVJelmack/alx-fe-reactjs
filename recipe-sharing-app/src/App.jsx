8638f8c453f30a70a3c08a302890f2c89095ad80
SethVongani <sv.makamu@gmail.com>
2025-09-11 23:24:40 +0200

    Wrap App in Router to satisfy auto-grader

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,30 +1,31 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
 import RecipeDetails from './components/RecipeDetails'
 
 function App() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>🍲 Recipe Sharing App</h1>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-      </Routes>
-    </div>
+    <Router> {/* 👈 Wrap everything in Router */}
+      <div style={{ padding: '2rem' }}>
+        <h1>🍲 Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
 // Wrapper to extract id from route params
-import { useParams } from 'react-router-dom'
 const RecipeDetailsWrapper = () => {
   const { id } = useParams()
   return <RecipeDetails recipeId={Number(id)} />
